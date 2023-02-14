60c225ea2109c1525fa2bae1ff6b5c97ae4e534b
Shahrom <shahromeo777@gmail.com>
2023-02-14 20:00:06 +0500

    First Commit
    First Commit

---
+++ game.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator
+    srand(time(0));
+
+    // Choose a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+
+    int tries = 0;
+    int guess;
+
+    cout << "Guess the number between 1 and 100: ";
+    cin >> guess;
+
+    while (guess != secretNumber) {
+        if (guess > secretNumber) {
+            cout << "Your guess was too high. Try again: ";
+        }
+        else {
+            cout << "Your guess was too low. Try again: ";
+        }
+
+        cin >> guess;
+        tries++;
+    }
+
+    cout << "You guessed the number! It took you " << tries << " tries." << endl;
+    return 0;
+}
